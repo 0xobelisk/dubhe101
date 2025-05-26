31249e6fd778fef2dfd9ffe95ef47b0bed12316b
v1xingyue <qixingyue@gmail.com>
2025-05-26 14:36:54 +0800

    add default index

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,8 +1,7 @@
-import { docs, meta } from "@/.source";
+import { docs } from "@/.source";
 import { loader } from "fumadocs-core/source";
-import { createMDXSource } from "fumadocs-mdx";
 
 export const source = loader({
   baseUrl: "/docs",
-  source: createMDXSource(docs, meta),
+  source: docs.toFumadocsSource(),
 });
